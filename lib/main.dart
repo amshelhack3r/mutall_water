e5dc7cf2651623a8be6943d95b27e25533087698
amshel <amshelhack3r@gmail.com>
2019-11-13 00:01:13 +0000

    Add a ChangeNotifierProvider to wrap Material application

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:mutall_water/State/ListProvider.dart';
+import 'package:provider/provider.dart';
 import './screens/selection.dart';
 
 
-void main() => runApp(MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(builder: (context)=>ListProvider(),
+    child: MaterialApp(
   debugShowCheckedModeBanner: false,
   theme: ThemeData(),
   title: "mutall meters",
   home: Selection(),
-));
-
-
+),);
+  }
+}
